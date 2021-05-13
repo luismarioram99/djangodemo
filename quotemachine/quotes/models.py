711cec4496fb9510807aa47cb34fe40f2fe0f915
jcusestabs <josecarlosramirezperalta@gmail.com>
2021-05-12 22:32:30 -0600

    Añadido Modelo Quote, registrado en el sitio Admin.

--- quotemachine/quotes/models.py
+++ quotemachine/quotes/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Quote(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(max_length=50, null=False, blank=False)

de4ed13cc083fecb524b64e7e5c35f417d7ed6c1
jcusestabs <josecarlosramirezperalta@gmail.com>
2021-05-14 15:18:38 -0600

    styled list create and login for application

--- quotemachine/quotes/models.py
+++ quotemachine/quotes/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 class Quote(models.Model):
     id      = models.IntegerField(primary_key=True, auto_created=True)
     author  = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.TextField(max_length=50, null=False, blank=False)
+    content = models.TextField(max_length=140, null=False, blank=False)
 
     def __str__(self):
         return "quote #" + str(self.id) + " by " + str(self.author)
